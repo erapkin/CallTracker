1a0a0c10a56bdfdea74898bc7d3499963a91d95d
Eli R <erapkin_be@dev8.devcodecamp.inc>
2015-11-04 16:56:59 -0600

    merged changes

--- CallTracker/Controllers/CallRecordsController.cs
+++ CallTracker/Controllers/CallRecordsController.cs
@@ -16,7 +16,11 @@ namespace CallTracker.Controllers
     public class CallRecordsController : ApiController
     {
         private CallTrackerContext db = new CallTrackerContext();
-
+        List<CallData> morningList = new List<CallData>();
+        List<CallData> earlyAfternoonList = new List<CallData>();
+        List<CallData> midDayList = new List<CallData>();
+        List<CallData> eveningList = new List<CallData>();
+        
         // GET: api/CallRecords
         public IQueryable<CallRecord> GetCallRecords()
         {
@@ -114,6 +118,78 @@ namespace CallTracker.Controllers
         private bool CallRecordExists(long id)
         {
             return db.CallRecords.Count(e => e.call_id == id) > 0;
+        }
+        public void StripTime(string call_id, string cust_id, string user_id, string call_day, DateTime call_time, bool available)
+        {
+            string timeString = call_time.ToString("HH");
+            int time = Int32.Parse(timeString);
+            if (available == true)
+            {
+                AddCallsToList(call_id, cust_id, user_id, call_day, time, available);
+            }
+        }
+
+        public void AddCallsToList(string call_id, string cust_id, string user_id, string call_day, int time, bool available)
+        {
+            CallData call = new CallData(call_id, cust_id, user_id, call_day, time, available);
+
+            int caseSwitch = time;
+
+            switch (caseSwitch)
+            {
+                case 7:
+                case 8:
+                case 9:
+                    morningList.Add(call);
+                    break;
+                case 10:
+                    earlyAfternoonList.Add(call);
+                    break;
+                case 11:
+                    earlyAfternoonList.Add(call);
+                    break;
+                case 12:
+                    earlyAfternoonList.Add(call);
+                    break;
+                case 13:
+                    midDayList.Add(call);
+                    break;
+                case 14:
+                    midDayList.Add(call);
+                    break;
+                case 15:
+                    midDayList.Add(call);
+                    break;
+                case 16:
+                    eveningList.Add(call);
+                    break;
+                case 17:
+                    eveningList.Add(call);
+                    break;
+                case 18:
+                    eveningList.Add(call);
+                    break;
+                case 19:
+                default:
+                    eveningList.Add(call);
+                    break;
+            }
+        }
+
+        public void CompareLists()
+        {
+            List<List<CallData>> allLists = new List<List<CallData>>();
+            allLists.Add(morningList);
+            allLists.Add(midDayList);
+            allLists.Add(earlyAfternoonList);
+            allLists.Add(eveningList);
+
+            var desc = from list in allLists orderby list descending select list;
+
+            foreach (List<CallData> list in desc)
+            {
+                Console.WriteLine(list);
+            }
         }
     }
 }
